1de36e9b01b2300f2c606a4b756264d5687d388b
Elias Buitrago Bolivar <eliasbuitrago1@gmail.com>
2020-08-25 22:27:17 -0500

    Add files via upload

---
+++ css/bootstrap-responsive.css
@@ -0,0 +1,60 @@
+@media screen and (min-width: 480px) and (max-width: 800px) {
+ .container, .navbar-static-top .container, .navbar-fixed-top .container, .navbar-fixed-bottom .container { width: 90%;}
+ .navbar .brand {margin-left:40%;}
+ .topbar div > ul > li, .nav > li {float:none;}
+ .nav-collapse.collapse {background:none;}
+ .hiremebadge {margin-left:-35%;}
+ .span16 { width: 97%;}
+ .topline {width:95%;}
+ .hero-unit h1 {font-size:30px;}
+ .hero-unit p {font-size:23px;}
+ .navbar .nav > li > a {font-size:14px;border-bottom: medium dashed #333333;}
+ .span-one-third { width: 30%;}
+ .footerend {width: 95%;}
+ 
+ }
+
+ @media screen and (max-width: 480px) {
+ .navbar .nav > li {float:none;}
+ .rightmenu {float:left !Important;}
+ .navbar .brand {float:right;margin-left:0px;}
+ .logo {margin-top:-60px;float:right;}
+ .navbar .nav > li > a {border-bottom: none;font-size:16px;}
+ .navbar .nav {clear:both;}
+ .hiremebadge { margin-left: 30%;  margin-right: 0; margin-top: -231px;}
+ .leftmenu {width:auto;}
+ .modal {width:80%;}
+ .menutitle {display:block;}
+ .hero-unit h1 {font-size:25px;line-height:35px;}
+ .hero-unit p {font-size:20px;line-height:30px;}
+ .span-one-third {width: 28%;}
+ .container, .navbar-static-top .container, .navbar-fixed-top .container, .navbar-fixed-bottom .container {  width: 86%;}
+ .modal-header {  padding: 5px 10px 5px 65px;}
+}
+@media screen and (min-width: 320px) and (max-width: 480px) { 
+ .nav-collapse.collapse {margin-top:20px;}
+}
+@media screen and (max-width: 320px) {
+ .nav-collapse.collapse {background:none;padding-top:160px}
+ .navbar .nav > li {float:none;}
+ .rightmenu {float:left !Important;}
+ .navbar .brand {margin-top:-310px;}
+ .logo {margin-top:-60px;float:right;}
+ .navbar .nav > li > a {border-bottom: none;font-size:14px;}
+ .navbar .nav {clear:both;}
+ .hiremebadge { margin-left: 30%;  margin-right: 0; margin-top: -231px;}
+ .leftmenu {width:auto;}
+ .modal {width:80%;}
+ .menutitle {display:block;}
+ .hero-unit h1 {font-size:25px;line-height:35px;}
+ .hero-unit p {font-size:20px;line-height:30px;}
+ .span-one-third {width: 28%;}
+ .container, .navbar-static-top .container, .navbar-fixed-top .container, .navbar-fixed-bottom .container {  width: 80%;}
+ .modal-header {  padding: 5px 10px 5px 65px;}
+ .span16 { width: 93%;}
+ .footerend {width:93%;}
+ .topline {width:93%;}
+ .span-one-third { width: 98%;}
+ .centermargin {margin-left: 0px;}
+ 
+}
